3d5bb1e05ec78649528b7d71095cd960e12dcfd7
Salah <salaheddinehanaoui@gmail.com>
2022-12-20 16:42:31 +0100

    Made a for loop that prints all the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,87 +1,3 @@
-//create the quiz questions
-let questions = [
-    {
-      question: "Why is AWS more economical than traditional data centers for applications with varying compute workloads?",
-      choiceA: "Amazon EC2 costs are billed on a monthly basis",
-      choiceB: "Users retain full administrative access to their Amazon EC2 instances.",
-      choiceC: "Amazon EC2 instances can be launched on demand when needed",
-      choiceD: "Users can permanently run enough instances to handle peak workloads.",
-      correct: "C",
-    },
-    {
-      question: "Which AWS service would simplify the migration of a database to AWS?",
-      choiceA: "AWS Storage Gateway",
-      choiceB: "AWS Database Migration Service (AWS DMS)",
-      choiceC: "Amazon EC2",
-      choiceD: "Amazon AppStream 2.0",
-      correct: "B",
-    },
-    {
-      question: " Which AWS offering enables users to find, buy, and immediately start using software solutions in their AWS environment?",
-      choiceA: "AWS Config",
-      choiceB: "AWS OpsWorks",
-      choiceC: "AWS SDK",
-      choiceD: "AWS Marketplace",
-      correct: "D",
-    },
-    {
-      question:"Which AWS networking service enables a company to create a virtual network within AWS?",
-      choiceA: "AWS Config",
-      choiceB: "Amazon Route 53",
-      choiceC: "AWS Direct Connect",
-      choiceD: "Amazon Virtual Private Cloud (Amazon VPC)",
-      correct: "D",
-    },
-    {
-      question: "Which of the following is an AWS responsibility under the AWS shared responsibility model?",
-      choiceA: "Configuring third-party applications",
-      choiceB: "Maintaining physical hardware",
-      choiceC: "Securing application access and data",
-      choiceD: "Managing guest operating systems",
-      correct: "B",
-    },
-    {
-      question: "Which component of the AWS global infrastructure does Amazon CloudFront use to ensure low-latency delivery?",
-      choiceA: "AWS Regions",
-      choiceB: "Edge locations",
-      choiceC: "Availability Zones",
-      choiceD: "Virtual Private Cloud (VPC)",
-      correct: "B",
-    },
-    {
-      question: "How would a system administrator add an additional layer of login security to a user's AWS Management Console?",
-      choiceA: "Use Amazon Cloud Directory",
-      choiceB: "Audit AWS Identity and Access Management (IAM) roles",
-      choiceC: "Enable multi-factor authentication",
-      choiceD: "Enable AWS CloudTrail",
-      correct: "C",
-    },
-    {
-      question: "Which service can identify the user that made the API call when an Amazon EC2 instance is terminated?",
-      choiceA: "AWS Trusted Advisor",
-      choiceB: "AWS CloudTrail",
-      choiceC: "AWS X-Ray",
-      choiceD: "AWS Identity and Access Management (AWS IAM)",
-      correct: "B",
-    },
-    {
-      question: "Which service would be used to send alerts based on Amazon CloudWatch alarms?",
-      choiceA: "Amazon Simple Notification Service (Amazon SNS)",
-      choiceB: "AWS CloudTrail",
-      choiceC: "AWS Trusted Advisor",
-      choiceD: "Amazon Route 53",
-      correct: "A",
-    },
-    {
-      question: "Where can a user find information about prohibited actions on the AWS infrastructure?",
-      choiceA: "AWS Trusted Advisor",
-      choiceB: "AWS Identity and Access Management (IAM)",
-      choiceC: "AWS Billing Console",
-      choiceD: "AWS Acceptable Use Policy",
-      correct: "D",
-    },
-];
-
 const previousBtn = document.getElementById("previousBtn");
 const nextBtn = document.getElementById("nextBtn");
 const finishBtn = document.getElementById("finishBtn");
@@ -143,14 +59,41 @@ function informationText(){
   
 }
 function quizzText(){
-//   text.innerText = questions[0]["question"];
-  text.innerHTML = `
-  <div class="form-check">
-  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-  <label class="form-check-label" for="flexCheckDefault">
-  ${questions[0]["choiceA"]}
-  </label>
-</div>`;
+    text.innerHTML = "";
+    for (let i = 0; i < questions.length; i++) {
+       text.innerHTML += `<section class="d-flex flex-wrap justify-content-center">
+  <div class="card m-2" style="width: 40rem;">
+      <div class="card-body">
+           <h5> ${questions[i]["question"]}</h5>
+          <div class="form-check">
+              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+              <label class="form-check-label" for="flexRadioDefault1">
+              ${questions[i]["choiceA"]} 
+              </label>
+          </div>
+          <div class="form-check">
+              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+              <label class="form-check-label" for="flexRadioDefault1">
+              ${questions[i]["choiceB"]}
+              </label>
+          </div>
+          <div class="form-check">
+              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+              <label class="form-check-label" for="flexRadioDefault1">
+              ${questions[i]["choiceC"]}
+              </label>
+          </div>
+          <div class="form-check">
+              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+              <label class="form-check-label" for="flexRadioDefault1">
+              ${questions[i]["choiceD"]}  
+              </label>
+          </div>
+      </div>
+  </div>
+</section>`; 
+      }
+  
   
 }
 function resultText(){
